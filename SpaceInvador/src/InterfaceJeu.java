eeabcbfef2b9a044f2d945e1c40fb6c0b483af76
Ilhy <anais.vannutelli@gmail.com>
2017-04-19 11:03:28 +0200

    petit test, modif juste sur interfacejeu

--- SpaceInvador/src/InterfaceJeu.java
+++ SpaceInvador/src/InterfaceJeu.java
@@ -1,5 +1,31 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class InterfaceJeu
+public class InterfaceJeu extends JFrame implements ActionListener
 {
+	JButton ajoutJoueur;
+	JLabel pseudo, niveau, score;
+	
+	public InterfaceJeu()
+	{
+		super("Super Space Invador");
+		
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new GridLayout(5,1));
+		
+		pack();
+		setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		
+	}
 	
 }
