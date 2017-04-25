e064f6e725b4a912aa66a6293ac95d28b6429979
Unknown <augesegolene@hotmail.fr>
2017-04-25 23:46:49 +0200

    Ligne / Missile / AjoutJoueur / Alien
    Bon tout à l'air de s'être modifier mais c'est faux c'est juste parce que j'ai réorganisé les trucs dans un dossier que ça fait ça ! Sinon j'ai modifier Alien en lui rajoutant un getScore(), j'ai modifié Missile en lui fait monter(), j'ai fait ligne (enfin je pense que ça doit marcher comme ça, j'ai pris listeBulle et j'ai adapté à Alien quoi ^^), et j'ai ajouté une image dans AjoutJoureur ! Voilou !

--- SpaceInvador/src/Alien.java
+++ SpaceInvador/src/Alien.java
@@ -1,3 +1,6 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 import java.util.Random;
 
@@ -8,7 +11,7 @@ public class Alien extends Ellipse2D.Double
 	public static final int DIAMETRE = 50;
 	int solidite;
 	
-	/* Constructeur */
+	// constructeur
 	public Alien(int centreX, int centreY)
 	{
 		super(centreX-DIAMETRE/2, centreY-DIAMETRE/2, DIAMETRE, DIAMETRE);
@@ -16,23 +19,30 @@ public class Alien extends Ellipse2D.Double
 		solidite = 1+r.nextInt(5);
 	}
 	
-	/* Pour avoir le rectangle autour du rond */
-	public boolean intersect(Alien a)
+	// Pour avoir le rectangle autour du rond
+	public boolean interect(Alien a)
 	{
 		return intersects(a.getBounds2D());
 	}
 
-	/* Pour descendre */
+	// Pour descendre
 	public void descendre()
 	{
 		super.y += PAS;
 	}
 	
-	/* Perte de solidité */
-	public void perteSolidite()
+	// Score rapporté par la mort de l'alien
+	public int getScore()
 	{
-		solidite --;  
+		return 1;
 	}
 	
+	/* Couleur Alien */
+	public void paint(Graphics2D g2)
+	{
+		g2.setStroke(new BasicStroke(2.0f));
+		g2.setColor(Color.GREEN);
+		g2.draw(this);
+	}
 	
 }
--- SpaceInvador/src/PanneauJeu.java
+++ SpaceInvador/src/PanneauJeu.java
@@ -1,4 +1,42 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PanneauJeu {
+import javax.swing.JComponent;
+import javax.swing.Timer;
 
+public class PanneauJeu extends JComponent implements ActionListener
+{
+	public static final int LARGEUR = 1000;
+	public static final int HAUTEUR = 900;
+	
+	Ligne listeAlien;
+	Vaisseau normandy;
+	Timer timerAlien, timerCreationAlien, timerMissile;
+	
+	
+	public PanneauJeu()
+	{
+		/* Ini du panneau avec une taille 400 (test) plus création des éléments présents  
+		 * initiallement dedans (ligne, vasseau) */
+		super();
+		setPreferredSize(new Dimension(LARGEUR, HAUTEUR));
+		listeAlien = new Ligne();
+		normandy  = new Vaisseau(195);
+		
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		
+		//listeAlien.paint(g2);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		
+	}
 }
