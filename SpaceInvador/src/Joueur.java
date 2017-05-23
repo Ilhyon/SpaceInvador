38c279814cdf8e02e25d21c643a42cbbebc4956c
Unknown <augesegolene@hotmail.fr>
2017-05-23 11:28:44 +0200

    Vaisseau, Missile, Joueur ont été commentés

--- SpaceInvador/src/Joueur.java
+++ SpaceInvador/src/Joueur.java
@@ -1,26 +1,19 @@
 /** Permet de stocker les information sur un joueur*/
 public class Joueur {
-	String pseudo;
-	int score;
+	String pseudo; // pseudo du joueur
+	int score; // son score
 	
+	// Constructeur si le joueur ne rentre pas de pseudo
 	Joueur()
 	{
-		score = 0;
 		pseudo = null;
+		score = 0;
 	}
 	
+	// Constructeur avec pseudo
 	Joueur(String p)
 	{
 		pseudo = p;
 		score = 0;
-	}
-	
-	public String toString()
-	{
-		StringBuffer buf = new StringBuffer();
-		buf.append(pseudo);
-		buf.append(" ");
-		buf.append(score);
-		return buf.toString();
 	}
 }
