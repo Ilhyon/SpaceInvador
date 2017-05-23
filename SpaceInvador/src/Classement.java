dbff9e0332a90146009879adec7a1c04b6f0e290
Unknown <augesegolene@hotmail.fr>
2017-05-23 12:04:29 +0200

    Classement commenté

--- SpaceInvador/src/Classement.java
+++ SpaceInvador/src/Classement.java
@@ -1,25 +1,27 @@
-import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.ListIterator;
 
 import javax.swing.table.AbstractTableModel;
 
 public class Classement extends AbstractTableModel{
-	ListeJoueur l;
+	ListeJoueur l; // Creation d'un objet listeJoueur
 
+	// Constructeur
 	public Classement(ListeJoueur l)
 	{
 		this.l = l;
 	}
 	
+	// Nombre de colonnes
 	public int getColumnCount() {
-		return 2; // une colonnes pour le score et une pour le pseudo ?
+		return 2; // une colonne pour le score et une pour le pseudo
 	}
 
+	// Nombre de ligne
 	public int getRowCount() {
-		return l.listeJoueur.size();
+		return l.listeJoueur.size(); // correspond à la taille de la liste
 	}
 	
+	// Noms des colonnes
 	public String getColumnName(int idC)
 	{
 		if(idC == 0)
@@ -32,30 +34,36 @@ public class Classement extends AbstractTableModel{
 		}
 	}
 	
-	
+	// Pour avoir les valeurs des cellules
 	public Object getValueAt(int idL, int idC) 
-	{
-		ListIterator<Joueur> iterJoueur = l.listeJoueur.listIterator();
-		String pseudo = null;
-		int score = 0;
-		Joueur j = new Joueur();
-		for(int i =0; i <= idL; i++)
+	{	
+		ListIterator<Joueur> iterJoueur = l.listeJoueur.listIterator(); // Iterateur de la liste
+		String pseudo = null; // pseudo vaut null
+		int score = 0; // score à 0
+		Joueur j = new Joueur(); // creation nouveau joueur
+		
+		// Pour i allant de 0 à indice de la ligne
+		for(int i = 0; i <= idL; i++)
 		{
-			j = iterJoueur.next();
-			pseudo = j.pseudo;
-			score = j.score;
+			j = iterJoueur.next(); // Le joueur devient le iteration suivante
+			pseudo = j.pseudo; // le pseudo du joueur devient le pseudo case
+			score = j.score; // le score du joueur devient le score de la case
 		}
 		
+		// la colonne 1 prendra le pseudo
 		if(idC == 0)
 		{
 			return pseudo;
 		}
+		
+		// Et la collone 2 le score
 		else
 		{
 			return score;
 		}
 	}
 	
+	// Pour retourner les valeurs des cellules des colonnes
 	public Class getColumnClass(int c) 
 	{ 
 		return getValueAt(0, c).getClass(); 
